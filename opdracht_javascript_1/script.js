a6df6b0e97daad777021fd8d06e3382299702961
Tim <71820486+TangoWhiskeyPapa@users.noreply.github.com>
2021-10-16 12:13:45 +0200

    Eerste JavaScript opdracht

---
+++ opdracht_javascript_1/script.js
@@ -0,0 +1,43 @@
+//opdracht 4
+console.log("Hello Winc Academy");
+
+//opdracht 5
+let mijnNaam = "Tim";
+
+console.log(mijnNaam);
+
+//opdracht 6
+//let if = "variabeleMetNaamIf"; Kan niet, if is een gereserveerde naam in JavaScript
+
+//opdracht 7
+let nummer1 = 4;
+let nummer2 = 4;
+
+console.log(nummer1 + nummer2); //Uitkomst is 8, deze variabelen kun je dus bij elkaar optellen
+
+let nummer3 = "4";
+let nummer4 = "4";
+
+console.log(nummer3 + nummer4); //Uitkomst is 44, deze strings kun je dus niet bij elkaar tellen maar ze worden dus als het ware achter elkaar geplakt.
+
+//opdracht 8
+let nummer5 = 12;
+let nummer6 = 6;
+
+console.log(nummer5 + nummer6); //Uitkomst is 18
+console.log(nummer5 - nummer6); //Uitkomst is 6
+console.log(nummer5 * nummer6); //Uitkomst is 72
+console.log(nummer5 / nummer6); //Uitkomst is 2
+console.log(nummer5 % nummer6); //Uitkomst is 0
+
+//opdracht 9
+let leeftijd = 30;
+
+console.log(typeof leeftijd); //Het type variabele is een number
+
+//opdracht 10
+//let leeftijd = "30"; //Console geeft een error aan dat ik al een variabele heb gedeclareerd met de naam leeftijd
+
+let leeftijdInLetters = "30";
+
+console.log(typeof leeftijdInLetters); //Het type variabele is een string
