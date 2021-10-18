a17a6ba6ebaff8e97d08e54fb6856d0e6fa45797
Tim <71820486+TangoWhiskeyPapa@users.noreply.github.com>
2021-10-18 15:26:31 +0200

    Tweede JavaScript opdracht

---
+++ opdracht_javascript_2/script.js
@@ -0,0 +1,14 @@
+//Dit is een grote som
+/*console.log(1230941 * 1823794);
+
+console.log(637263 / 54);*/
+
+/* ------ANTWOORDEN OP DE VRAGEN-----
+
+4. Run je code met node. Zie je de uitkomst van je comment in de console? Zo ja, waarom? Zo nee, waarom niet?
+Antwoord: Nee, want de comment wordt niet uitgevoerd in JavaScript.
+
+6. Run je code opnieuw met node. Zie je de uitkomst van de som nu nog steeds in de console? Waarom wel/niet?
+Antwoord: Nee, want beide sommen zijn nu commentaar geworden. Deze worden niet gerund door JavaScript en verschijnen dus niet in je node command prompt
+
+*/
