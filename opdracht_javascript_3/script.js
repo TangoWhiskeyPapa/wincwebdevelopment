c5ca653caa5a8a554bbe757d1b206eb6aee4af36
Tim <71820486+TangoWhiskeyPapa@users.noreply.github.com>
2021-10-19 13:49:25 +0200

    Derde Javascript opdracht

---
+++ opdracht_javascript_3/script.js
@@ -0,0 +1,21 @@
+const age = 30;
+const isFemale = false;
+const driverStatus = "bob";
+
+if(age >= 18){
+    console.log("Je bent oud genoeg om naar binnen te mogen");
+}else{
+    console.log("Je bent helaas te jong om de kroeg te betreden");
+}
+
+if(isFemale){
+    console.log("Who run the world? Girls!")
+}else{
+    console.log("Sorry, deze avond is alleen voor vrouwen bedoelt! ;-)");
+}
+
+if(driverStatus == "bob"){
+    console.log("Jij bent de bob dus jij kan veilig naar huis rijden!");
+}else{
+    console.log("Jij bent niet de bob vanavond dus je mag helaas niet naar huis rijden!");
+}
